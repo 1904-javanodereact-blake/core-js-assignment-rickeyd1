b668ff637a35f95bc2a596948858908f0e6df9e8
Ric Davis <rickjay0@gmail.com>
2019-04-02 19:11:58 -0400

    Finished the fib using loops

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,17 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    let temp = 1;
+    let secondTemp;
+    var result = 1;
+    if(n < 3){
+        return result;
+    } else {
+        for(let i = 3; i < n+1; i++){
+            secondTemp = temp;
+            temp = result;
+            result = temp + secondTemp;
+        }
+        return result;   
+    }
 }
