ae2152cfe8537b26f19e68de2628898707d7df22
Ric Davis <rickjay0@gmail.com>
2019-04-02 20:37:31 -0400

    Finished the bubbleSort

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,9 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let revStr = "";
+    for(let i = someStr.length-1; i >= 0; i--){
+        revStr += someStr[i];
+    }
+    return revStr;
 }
