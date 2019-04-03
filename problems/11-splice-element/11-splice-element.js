481a312b9f901256be6f4b20d24e923d3f1600a5
Ric Davis <rickjay0@gmail.com>
2019-04-03 12:45:06 -0400

    Testing VS Code

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,7 +3,8 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+  let we = 0;
 }
 
 // Remove element at the specified index.
+//Stuff
