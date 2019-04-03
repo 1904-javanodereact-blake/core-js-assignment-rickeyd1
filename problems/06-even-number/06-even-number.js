7abf3bf3dd0a189205489778600cf5c6780b5d43
Ric Davis <rickjay0@gmail.com>
2019-04-02 22:36:22 -0400

    Finished even number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,12 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    let diff;
+    while(someNum > -1){           
+        someNum -= 2;
+        if(someNum === 0){
+            return true;
+        }
+    }
+    return false;
 }
