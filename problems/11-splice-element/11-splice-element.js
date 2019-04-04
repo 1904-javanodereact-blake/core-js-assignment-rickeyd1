8c13da7bdd8026db577b5efcb6bbc9bb20bf9fa9
Ric Davis <rickjay0@gmail.com>
2019-04-04 13:24:15 -0400

    Finished splice

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,8 +3,16 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-    
+    if(index === 0){
+        someArr.shift();
+    } else if (index === (someArr.length) - 1){
+        someArr.pop();
+    } else if (index >= someArr.length) {
+        console.log("Entered an index outside of the array.");      // Checks if index is outside of array and returns message.
+    } else {
+        for(let i = index; i < someArr.length; i++){
+            someArr[i] = someArr[i+1];
+        }
+        someArr.pop();
+    }
 }
-
-// Remove element at the specified index.
-//Stuff
