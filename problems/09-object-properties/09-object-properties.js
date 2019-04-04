b3ccbf58f0bad36b963cfc66f3eb0d7346eab0b2
Ric Davis <rickjay0@gmail.com>
2019-04-04 11:41:37 -0400

    Finished object properties

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,15 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    for (let k in someObj) {
+        console.log(`${k}: ${someObj[k]}`);
+    }
 }
+
+ let myCake = {
+    name: 'Ice Cream',
+    layers: 5,
+    rating: 10
+ }
+
+ objectProperties(myCake);
