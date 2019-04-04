72a4ed8ddea6c45519cdb10247464b995768cde7
Ric Davis <rickjay0@gmail.com>
2019-04-03 20:40:03 -0400

    Finished shapes and added formatting for diamond

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -22,37 +22,61 @@ Example for printShape("Diamond", 5, "*");
 */
 function printShape(shape, height, character) {
   let resStr = "";
-  switch(shape)
-  {
-    case 'Square':
-      for(let i = 0; i < height; i++){
-        resStr = "";
-        for(let j = 0; j < height; j++){
-          resStr += character;
+  if((height % 2) === 0){
+    console.log("Please enter an odd number.");
+  } else { 
+    switch(shape)
+    {
+      case 'Square':
+        for(let i = 0; i < height; i++){
+          resStr = "";
+          for(let j = 0; j < height; j++){
+            resStr += character;
+          }
+          console.log(resStr);
         }
-        console.log(resStr);
-      }
-      break;
-    case 'Triangle':
-      for(let i = 0; i < height; i++){
-        resStr = "";
-        for(let j = 0; j <= i; j++){
-          resStr += character;
+        break;
+      case 'Triangle':
+        for(let i = 0; i < height; i++){
+          resStr = "";
+          for(let j = 0; j <= i; j++){
+            resStr += character;
+          }
+          console.log(resStr);
         }
-        console.log(resStr);
-      }
-      break;
-    case 'Diamond':
-      break;
-    default:
-      console.log("Incorrect input entered");
+        break;
+      case 'Diamond':
+        let resStrUp;
+        let resStrDown;
+        let counter = 0;
+        let halfLen = Math.floor(height/2);
+        for(let i = 1; i < height; i += 2){1
+          resStrUp = "";
+
+          for(let a = 0; a < halfLen; a++){
+            resStrUp += " ";
+          }
+
+          halfLen--;
+          for(let u = 0; u < i; u++){
+            resStrUp += character;
+          }
+          console.log(resStrUp);
+        }
+        for(let j = height; j > 0; j -= 2){
+          resStrDown = "";
+          for(let b = 0; b < counter; b++){
+            resStrDown += " ";
+          }
+          counter++;
+          for(let d = 0; d < j; d++){
+            resStrDown += character;
+          }
+          console.log(resStrDown);
+        }
+        break;
+      default:
+        console.log("Incorrect input entered");
+    }
   }
 }
-
-let mySquare = 'Square';
-let myTriangle = 'Triangle';
-let myDiamond = 'Diamond';
-
-//printShape(mySquare, 3, 'h');
-//printShape(myTriangle, 3, ' $ ');
-//printShape(myDiamond, 3, '*');
